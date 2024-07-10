8e70a6522bc348acff381c1ff88603dbf6ce47ee
MohammedAbdElfatah0 <hamoabdelfatah611@gmail.com>
2024-07-10 17:55:14 +0300

    first commit

---
+++ note_app/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/screen/note_app.dart';
+
+void main() {
+  runApp(const NoteApp());
+}
---
+++ note_app/lib/screen/note_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/screen/home_view.dart';
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      home:const HomeView(),
+    );
+  }
+}
